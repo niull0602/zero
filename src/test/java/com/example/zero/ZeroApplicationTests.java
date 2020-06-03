11516641cd1687d1555520084c2af8af08b2a31b
Administrator <lelement@163.com>
2020-06-04 00:22:17 +0800

    first

---
+++ src/test/java/com/example/zero/ZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zero;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroApplicationTests {
+
+    void contextLoads() {
+
+    }
+
+}
